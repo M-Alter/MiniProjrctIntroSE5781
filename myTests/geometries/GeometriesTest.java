76b9c7723f93b851fb688c47e68f1070d3ebb866
Menachem <alter.menachem@gmail.com>
2021-05-02 21:24:07 +0300

    Targil 3

--- myTests/geometries/GeometriesTest.java
+++ myTests/geometries/GeometriesTest.java
@@ -34,27 +34,6 @@ class GeometriesTests {
         assertEquals( 4, geometries.findIntersections(new Ray(new Point3D(0.5, 6, 0.5), new Vector(0, -1, 0))).size(),("Only one shape is cut"));
 
 
-//        Geometries geometries = new Geometries();
-//        geometries.add(new Sphere(new Point3D(1,1,1), 1d));
-//        geometries.add(new Triangle(
-//                new Point3D(-2,4,2.5),
-//                new Point3D(-4,-3,2.5),
-//                new Point3D(3,-3,2.5)));
-//        geometries.add(new Plane(
-//                new Point3D(2,2,3.5),
-//                new Point3D(-2,2,3.5),
-//                new Point3D(-2,-2,3.5)));
-//        List<Point3D> result = geometries.findIntersections(
-//                new Ray(new Point3D(2,2,0),
-//                        new Vector(-2,-1,3)));
-//        assertEquals(4,result.size());
-//        Sphere sphere = new Sphere(new Point3D(1,1,1), 1d);
-//        List<Point3D> result = sphere.findIntersections(new Ray(new Point3D(2,2,0), new Vector(-2,-1,3)));
-//        assertEquals(2,result.size());
-//        new Triangle(
-//                new Point3D(3,3,4),
-//                new Point3D(1,-2,4),
-//                new Point3D(-2,3,4));
-//
+
     }
 }
